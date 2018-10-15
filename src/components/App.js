b6e204a01ee28e2efa1e9e532183d28ad71e4ff2
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-15 12:28:19 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import NavBar from './NavBar'
+import GifListContainer from '../containers/GifListContainer'
 
-// the App component should render out the GifListContainer component 
+// the App component should render out the GifListContainer component
+//  this is how to limit the page to a certain amount ?_limit=50&_page=${c}
 
 function App() {
   return (
     <div>
         < NavBar color='black' title="Giphy Search" />
+      <GifListContainer />
     </div>
   )
 }
